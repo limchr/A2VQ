b5ae21809c5d14be530649e775eda555bc20bd8c
Chris <chris@email.address>
2020-06-08 10:35:08 +0200

    fixed deprecated imresize from scipy"

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -96,7 +96,7 @@ def get_deep_feats(imgs, use_model='vgg16', pca_dim=None):
     return rtn_feats
 
 def preprocess_image(img, imgsize):
-    img_load = scipy.misc.imresize(img, imgsize)
+    img_load = np.array(Image.fromarray(img).resize(imgsize[:2]))
     img_data = image.img_to_array(img_load)
     img_data = np.expand_dims(img_data, axis=0)
     img_data = preprocess_input(img_data, dim_ordering='tf')
@@ -125,9 +125,8 @@ def resize_image_tuple_to_max_edge_length(imgs, max_edge_length=200):
         else:
             w = max_edge_length
             h = int((float(s[0])/s[1]) * max_edge_length)
-        rtn_imgs.append(scipy.misc.imresize(img, (h,w,s[2])))
+        rtn_imgs.append(np.array(Image.fromarray(img).resize((h,w))))
     return rtn_imgs
-
 
 
 
